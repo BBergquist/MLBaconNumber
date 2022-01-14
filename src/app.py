aea7ec642fa59689ca886bcaca8cd822f17a2d9e
Blake Bergquist <blake.bergquist.1995@gmail.com>
2022-01-14 02:44:19 +0000

    devcontainer setup

---
+++ src/app.py
@@ -0,0 +1,10 @@
+# Sample Code - just press F5
+
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+
+df = spark.createDataFrame([
+    ("10", "Bob"), ("11", "JimBob"), ("13", "Bobby")
+], ["age", "Name"])
+
+df.show(20, False)
